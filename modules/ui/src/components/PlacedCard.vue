bce7a40628767b8d4dec4c63142394c066b4996d
Élise Rubio <elise.rubio@4sh.fr>
2021-07-31 22:45:24 +0200

    :lipstick: Add border to card

--- modules/ui/src/components/PlacedCard.vue
+++ modules/ui/src/components/PlacedCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <div v-for="idea in card.ideas">
-    {{idea}}
+  <div class="card">
+    <div v-for="idea in card.ideas">
+      {{ idea }}
+    </div>
   </div>
 </template>
 
@@ -19,5 +21,10 @@ export default {
 </script>
 
 <style scoped>
-
+.card {
+  border: 4px solid #2c3e50;
+  width:200px;
+  height:200px;
+  margin: auto auto 10px;
+}
 </style>
