281bf7a00007c279e8f9ade3d56b7df8431ca283
Élise Rubio <elise.rubio@4sh.fr>
2021-08-14 21:47:08 +0200

    :sparkles: Display all four ideas of a card
    Co-authored-by: Tom Pietri <to.pietri@gmail.com>

--- modules/ui/src/components/PlacedCard.vue
+++ modules/ui/src/components/PlacedCard.vue
@@ -1,7 +1,10 @@
 <template>
   <v-group :config="{draggable:true}">
     <v-rect :config="cardConfig"></v-rect>
-    <v-text :config="ideaConfig"></v-text>
+    <v-text :config="idea1Config"></v-text>
+    <v-text :config="idea2Config"></v-text>
+    <v-text :config="idea3Config"></v-text>
+    <v-text :config="idea4Config"></v-text>
   </v-group>
 </template>
 
@@ -17,15 +20,36 @@ export default {
         y: 100,
         width:100,
         height:100,
-        fill: 'red',
+        fill: 'white',
         stroke: 'black'
       },
-      ideaConfig: {
+      idea1Config: {
         text: this.card.ideas[0],
         fontSize: 15,
         x:325,
-        y:100
-      }
+        y:103
+      },
+      idea2Config: {
+        text: this.card.ideas[1],
+        fontSize: 15,
+        x:397,
+        y:125,
+        rotation:90
+      },
+      idea3Config: {
+        text: this.card.ideas[2],
+        fontSize: 15,
+        x:375,
+        y:197,
+        rotation:180
+      },
+      idea4Config: {
+        text: this.card.ideas[3],
+        fontSize: 15,
+        x:303,
+        y:175,
+        rotation:270
+      },
     }
   },
   props: {
